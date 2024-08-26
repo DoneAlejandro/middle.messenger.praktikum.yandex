fe6c7ed262d7d5f67bb9ded97f054066e03e0523
Алексей Болдырев <84848493+DoneAlejandro@users.noreply.github.com>
2024-08-26 22:42:56 +0300

    починен рендер, нужно переписать оставшуюся логику

--- src/components/header/Header.ts
+++ src/components/header/Header.ts
@@ -1,16 +1,21 @@
 import Block from '../../parentClasses/Block/BLock';
 import { TBlock } from '../../parentClasses/types';
+import { Title } from '../title';
 import headerTemplate from './header.hbs?raw';
 
 export class Header extends Block {
 	constructor(props: TBlock) {
-		super(props);
+		super({...props, title});
 	}
 	render() {
 		console.log(`header ${headerTemplate} this.compile(headerTemplate, this.props) ${this.compile(headerTemplate, this.props)}`);
-		// return this.compile(headerTemplate, this.props);
-		return `
-            <div class="input__text-error">{{errorText}}</div>
-        `;
+		return this.compile(headerTemplate, this.props);
+		// return `
+        //     <div class="input__text-error">{{errorText}}</div>
+        // `;
 	}
 }
+
+export const title = new Title({
+	titleText: 'ONE-on-ONE Social',
+})
--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,20 +1,15 @@
-import { Header, Title } from '../../components';
+import { Header } from '../../components';
 import Block from '../../parentClasses/Block/BLock';
 import { TBlock } from '../../parentClasses/types';
 import SigninPage from './signin.hbs?raw';
 
 export class SignIn extends Block {
 	constructor(props: TBlock) {
-		super(props);
+		super({ ...props, header });
 	}
 	render() {
 		return this.compile(SigninPage, this.props);
 	}
 }
 
-export const headerT = new Header({
-	title: new Title({
-		title: 'ONE-on-ONE Social',
-	})
-	
-});
+export const header = new Header({});
