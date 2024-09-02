6edbfc491a67a94ec411bc9796b7c69b2ed456da
Алексей Болдырев <84848493+DoneAlejandro@users.noreply.github.com>
2024-09-02 19:43:32 +0300

    добавлены компоненты

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,4 +1,4 @@
-import { Form, Header, Main, Title } from '../../components';
+import { Form, Header, Input, InputField, Main, Title, Button } from '../../components';
 import Block from '../../parentClasses/Block/BLock';
 import { TBlock } from '../../parentClasses/types';
 import { Subtitle } from './../../components/subtitle/Subtitle';
@@ -14,10 +14,41 @@ export class SignIn extends Block {
 				}),
 			}),
 			mainComponent: new Main({
+				mainStyle: 'main-signin',
+
 				formComponent: new Form({
+					formStyle: 'signin-form',
+					signinStyle: 'signin',
+					formContainerStyle: 'signin-form__container',
+					formInputsStyle: 'signin-form__inputs',
 					subtitleComponent: new Subtitle({
 						subtitleText: 'Войти',
 					}),
+					InputFieldLoginComponent: new InputField({
+						inputFieldStyle: 'input-field',
+						inputFieldLabelStyle: 'input-field__label',
+						labelInput: 'Логин',
+						labelTitle: 'Логин',
+						inputComponent: new Input({
+							inputType: 'text',
+							inputTitle: 'Логин',
+							inputName: 'login',
+						}),
+					}),
+					InputFieldPasswordComponent: new InputField({
+						inputFieldStyle: 'input-field',
+						inputFieldLabelStyle: 'input-field__label',
+						labelInput: 'Пароль',
+						labelTitle: 'Пароль',
+						inputComponent: new Input({
+							inputType: 'password',
+							inputTitle: 'Пароль',
+							inputName: 'password',
+						}),
+					}),
+					buttonComponent: new Button({
+						
+					})
 				}),
 			}),
 		});
