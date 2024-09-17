4d128fb050d25bfcafc852425e75512898be9cb5
Алексей Болдырев <84848493+DoneAlejandro@users.noreply.github.com>
2024-09-17 23:04:12 +0300

    внесены доработки по роутеру

--- src/requests/requests.ts
+++ src/requests/requests.ts
@@ -1,96 +1,96 @@
-// // Объект, содержащий допустимые HTTP-методы
-// const METHODS = {
-// 	GET: 'GET',
-// 	POST: 'POST',
-// 	PUT: 'PUT',
-// 	DELETE: 'DELETE',
-// } as const;
+// Объект, содержащий допустимые HTTP-методы
+const METHODS = {
+	GET: 'GET',
+	POST: 'POST',
+	PUT: 'PUT',
+	DELETE: 'DELETE',
+} as const;
 
-// // Тип, представляющий допустимые значения методов из объекта METHODS
-// type MethodType = (typeof METHODS)[keyof typeof METHODS];
+// Тип, представляющий допустимые значения методов из объекта METHODS
+type MethodType = (typeof METHODS)[keyof typeof METHODS];
 
-// // Интерфейс, описывающий возможные параметры запроса
-// interface RequestOptions {
-// 	method?: MethodType;
-// 	headers?: Record<string, string>;
-// 	data?: Record<string, unknown> | string;
-// 	timeout?: number;
-// }
+// Интерфейс, описывающий возможные параметры запроса
+interface RequestOptions {
+	method?: MethodType;
+	headers?: Record<string, string>;
+	data?: Record<string, unknown> | string;
+	timeout?: number;
+}
 
-// // Функция для преобразования объекта в строку запроса (query string)
-// function queryStringify(data: Record<string, unknown>): string {
-// 	const keys = Object.keys(data);
-// 	return keys.reduce((result, key: string, index): string => {
-// 		const value = data[key];
-// 		const encodedValue = encodeURIComponent(value !== undefined ? String(value) : '');
-// 		return `${result}${key}=${encodedValue}${index < keys.length - 1 ? '&' : ''}`;
-// 	}, '?');
-// }
+// Функция для преобразования объекта в строку запроса (query string)
+function queryStringify(data: Record<string, unknown>): string {
+	const keys = Object.keys(data);
+	return keys.reduce((result, key: string, index): string => {
+		const value = data[key];
+		const encodedValue = encodeURIComponent(value !== undefined ? String(value) : '');
+		return `${result}${key}=${encodedValue}${index < keys.length - 1 ? '&' : ''}`;
+	}, '?');
+}
 
-// // Класс для выполнения HTTP-запросов
-// class HTTPTransport {
-// 	// Метод для выполнения GET-запроса
-// 	get = (url: string, options: RequestOptions = {}) => {
-// 		return this.request(url, { ...options, method: METHODS.GET }, options.timeout);
-// 	};
+// Класс для выполнения HTTP-запросов
+class HTTPTransport {
+	// Метод для выполнения GET-запроса
+	get = (url: string, options: RequestOptions = {}) => {
+		return this.request(url, { ...options, method: METHODS.GET }, options.timeout);
+	};
 
-// 	// Метод для выполнения POST-запроса
-// 	post = (url: string, options: RequestOptions = {}) => {
-// 		return this.request(url, { ...options, method: METHODS.POST }, options.timeout);
-// 	};
+	// Метод для выполнения POST-запроса
+	post = (url: string, options: RequestOptions = {}) => {
+		return this.request(url, { ...options, method: METHODS.POST }, options.timeout);
+	};
 
-// 	// Метод для выполнения PUT-запроса
-// 	put = (url: string, options: RequestOptions = {}) => {
-// 		return this.request(url, { ...options, method: METHODS.PUT }, options.timeout);
-// 	};
+	// Метод для выполнения PUT-запроса
+	put = (url: string, options: RequestOptions = {}) => {
+		return this.request(url, { ...options, method: METHODS.PUT }, options.timeout);
+	};
 
-// 	// Метод для выполнения DELETE-запроса
-// 	delete = (url: string, options: RequestOptions = {}) => {
-// 		return this.request(url, { ...options, method: METHODS.DELETE }, options.timeout);
-// 	};
+	// Метод для выполнения DELETE-запроса
+	delete = (url: string, options: RequestOptions = {}) => {
+		return this.request(url, { ...options, method: METHODS.DELETE }, options.timeout);
+	};
 
-// 	// Метод для выполнения HTTP-запроса
-// 	request = (url: string, options: RequestOptions = {}, timeout = 5000): Promise<XMLHttpRequest> => {
-// 		const { headers = {}, method, data } = options;
+	// Метод для выполнения HTTP-запроса
+	request = (url: string, options: RequestOptions = {}, timeout = 5000): Promise<XMLHttpRequest> => {
+		const { headers = {}, method, data } = options;
 
-// 		return new Promise<XMLHttpRequest>((resolve, reject) => {
-// 			// Проверяем, указан ли метод
-// 			if (!method) {
-// 				reject(new Error('No method provided'));
-// 				return;
-// 			}
+		return new Promise<XMLHttpRequest>((resolve, reject) => {
+			// Проверяем, указан ли метод
+			if (!method) {
+				reject(new Error('No method provided'));
+				return;
+			}
 
-// 			const xhr = new XMLHttpRequest();
-// 			const isGet = method === METHODS.GET;
+			const xhr = new XMLHttpRequest();
+			const isGet = method === METHODS.GET;
 
-// 			// Открываем соединение
-// 			xhr.open(method, isGet && !!data ? `${url}${queryStringify(data as Record<string, unknown>)}` : url);
+			// Открываем соединение
+			xhr.open(method, isGet && !!data ? `${url}${queryStringify(data as Record<string, unknown>)}` : url);
 
-// 			// Устанавливаем заголовки запроса
-// 			Object.keys(headers).forEach(key => {
-// 				xhr.setRequestHeader(key, headers[key]);
-// 			});
+			// Устанавливаем заголовки запроса
+			Object.keys(headers).forEach(key => {
+				xhr.setRequestHeader(key, headers[key]);
+			});
 
-// 			// Устанавливаем обработчик на событие завершения загрузки
-// 			xhr.onload = function () {
-// 				resolve(xhr);
-// 			};
+			// Устанавливаем обработчик на событие завершения загрузки
+			xhr.onload = function () {
+				resolve(xhr);
+			};
 
-// 			// Обработчики на события ошибки, прерывания и таймаута
-// 			xhr.onabort = () => reject(new Error('Request aborted'));
-// 			xhr.onerror = () => reject(new Error('Request failed'));
-// 			xhr.ontimeout = () => reject(new Error('Request timed out'));
+			// Обработчики на события ошибки, прерывания и таймаута
+			xhr.onabort = () => reject(new Error('Request aborted'));
+			xhr.onerror = () => reject(new Error('Request failed'));
+			xhr.ontimeout = () => reject(new Error('Request timed out'));
 
-// 			xhr.timeout = timeout;
+			xhr.timeout = timeout;
 
-// 			// Отправляем запрос
-// 			if (isGet || !data) {
-// 				xhr.send();
-// 			} else if (typeof data === 'string') {
-// 				xhr.send(data);
-// 			} else {
-// 				xhr.send(JSON.stringify(data));
-// 			}
-// 		});
-// 	};
-// }
+			// Отправляем запрос
+			if (isGet || !data) {
+				xhr.send();
+			} else if (typeof data === 'string') {
+				xhr.send(data);
+			} else {
+				xhr.send(JSON.stringify(data));
+			}
+		});
+	};
+}
